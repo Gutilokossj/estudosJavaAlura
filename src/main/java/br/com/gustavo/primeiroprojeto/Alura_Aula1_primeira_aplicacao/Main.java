02ba78b58cdc61df3309f73916442bada2d2fca7
Gutilokossj <gutilokossj12@gmail.com>
2025-02-07 15:47:10 -0300

    Salvando o começo dos estudos

---
+++ src/main/java/br/com/gustavo/primeiroprojeto/Alura_Aula1_primeira_aplicacao/Main.java
@@ -0,0 +1,7 @@
+package br.com.gustavo.primeiroprojeto.Alura_Aula1_primeira_aplicacao;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Alo mundo! agora vai em");
+    }
+}
---
+++ src/main/java/br/com/gustavo/primeiroprojeto/Alura_Aula1_primeira_aplicacao/Perfil.java
@@ -0,0 +1,4 @@
+package br.com.gustavo.primeiroprojeto.Alura_Aula1_primeira_aplicacao;
+
+public class Perfil {
+}
